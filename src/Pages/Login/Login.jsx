21046b0b605b628a1731ddde03a06bf93bd6e384
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-06-03 23:33:30 +0600

    bug fixed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,21 +10,21 @@ const Login = () => {
                     <Form action="" className='flex flex-col gap-5'>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor="email">Email</label>
-                            <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full w-[300px]" />
+                            <input type="text" placeholder="Type here" className="input input-bordered input-warning w-[400px]" />
                         </div>
                         <div className='flex flex-col gap-1'>
                             <label htmlFor="password">Password</label>
-                            <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full w-[300px]" />
+                            <input type="text" placeholder="Type here" className="input input-bordered input-warning w-[400px]" />
                         </div>
-                        <div>
-                            <input type="submit" className='btn btn-md px-10' value="login" />
+                        <div className='mt-8'>
+                            <input type="submit" className='btn btn-md btn-info px-10' value="login" />
                         </div>
                     </Form>
                 </div>
                 <div className='flex flex-col gap-10 px-10 text-white bg-[#f0be61] w-4/12 text-center justify-center items-center'>
                     <h1 className='text-5xl font-bold'>New Here ?</h1>
                     <p>Sign up and discover a great amount of new opportunities</p>
-                    <Link to='/signup' className='btn border-none rounded-md bg-white w-40'>Sign Up</Link>
+                    <Link to='/signup' className='btn border-none rounded-md btn-info w-40'>Sign Up</Link>
                 </div>
             </div>
         </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -9,25 +9,25 @@ const SignUp = () => {
                 <Form action="" className='flex flex-col gap-5'>
                     <div className='flex flex-col gap-1'>
                         <label htmlFor="email">Name</label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full w-[300px]" />
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-[400px]" />
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label htmlFor="email">Email</label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full w-[300px]" />
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-[400px]" />
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label htmlFor="password">Password</label>
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-full w-[300px]" />
+                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-[400px]" />
                     </div>
-                    <div>
-                        <input type="submit" className='btn btn-md px-10 bg-white' value="Sign Up" />
+                    <div className='mt-5'>
+                        <input type="submit" className='btn btn-md px-8 btn-info' value="Sign Up" />
                     </div>
                 </Form>
             </div>
             <div className='flex flex-col gap-10 px-5 bg-white text-[#F0BE61]  w-4/12 text-center justify-center items-center'>
                 <h1 className='text-5xl font-bold'>Have an account ?</h1>
                 <p>If you already have na account you have to login</p>
-                <Link to='/login' className='btn border-none rounded-md bg-white w-40 bg-[#f0be61]'>Login</Link>
+                <Link to='/login' className='btn border-none rounded-md btn-info px-8'>Login</Link>
             </div>
         </div>
     );
