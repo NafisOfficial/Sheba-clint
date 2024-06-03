95c47dc42fdecc97fe5d2c2d8968a671da85e84a
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-06-03 11:18:14 +0600

    Set folder structure, layout, routes

---
+++ src/Layout/Dashbord/Dashbord.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashbord = () => {
+    return (
+        <div>
+            Dashbord Layout
+        </div>
+    );
+};
+
+export default Dashbord;
---
+++ src/Provider/RouterProvider/Routes.jsx
@@ -0,0 +1,23 @@
+
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Main from "../../Layout/Main/Main";
+import Dashbord from "../../Layout/Dashbord/Dashbord";
+import Info from "../../Layout/Info/Info";
+
+
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+    },
+    {
+        path: "/dashbord",
+        element: <Dashbord/>
+    },
+    {
+      path: "/info",
+      element:<Info/>
+    }
+  ]);
