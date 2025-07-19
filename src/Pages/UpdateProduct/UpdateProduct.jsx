c6b1fb11b39b327d97484f5093b169fad7f4bea6
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-07-20 00:47:18 +0600

    update product route clear

--- src/Pages/UpdateProduct/UpdateProduct.jsx
+++ src/Pages/UpdateProduct/UpdateProduct.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
-import { Form, useLoaderData } from "react-router-dom";
+import { Form, useLoaderData, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 // import { toast } from "react-toastify";
 
 
@@ -8,30 +9,63 @@ import { Form, useLoaderData } from "react-router-dom";
 const UpdateProduct = () => {
     const [open, setOpen] = useState(false);
     const data = useLoaderData();
-    console.log(data);
-    const { image, brand, dose, form, generic, company_name, price_per_unit, indications, interaction, mode_of_action, precautions_and_warnings, pregnancy_and_lactation, side_effects } = data
+    const navigate = useNavigate();
+
+    const { _id,image, brand, dose, form, generic, company_name, price_per_unit, indications, interaction, mode_of_action, precautions_and_warnings, pregnancy_and_lactation, side_effects } = data
+
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+
+        const target = event.target;
+        const brand = target.brand.value || "";
+        const dose = target.dose.value || "";
+        const form = target.form.value || "";
+        const company_name = target.company_name.value || "";
+        const generic = target.generic.value || "";
+        const price_per_unit = target.price_per_unit.value || "";
+        const unit_per_strip = target.pack_size_and_price.value || "";
+        const indications = target?.indications?.value || "";
+        const mode_of_action = target?.mode_of_action?.value || "";
+        const side_effects = target?.side_effects?.value || "";
+        const precautions_and_warnings = target?.precautions_and_warnings?.value || "";
+        const pregnancy_and_lactation = target?.pregnancy_and_lactation?.value || "";
+        const interaction = target?.interaction?.value || "";
+        
+        const product = {brand,dose,form,company_name,generic, price_per_unit, unit_per_strip, indications, mode_of_action, side_effects, precautions_and_warnings, pregnancy_and_lactation, interaction};
+
+
+        fetch(`http://localhost:3000/drugs/update/${_id}`,{
+                    method: "PATCH",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify(product)
+                }).then(()=>{
+                    navigate(`/dashboard/details/${_id}`);
+                    toast.success("Data updated");
+                }).catch(()=>{
+                    toast.error("Filed to update data");
+                })
+    }
 
     return (
         <div className="flex justify-center items-center w-full h-full py-10">
-            <Form action="" className="border border-4 border-warning p-5 rounded flex flex-col gap-2">
+            <Form onSubmit={handleSubmit} className="border border-4 border-warning p-5 rounded flex flex-col gap-2">
                 <div className="text-center">
                     <p className="my-5 font-bold text-2xl">Update medicine data</p>
                 </div>
                 <div>
                     <img src={image} alt="product" className="h-32 w-32" />
-                    <div className="form-control">
-                        <label className="label"><span className="label-text font-semibold">Change Image</span></label>
-                        <input required type="file" name="image" className="file-input file-input-bordered file-input-warning" />
-                    </div>
                 </div>
                 <div className="flex gap-2">
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text font-semibold">Brand</span></label>
-                        <input required type="text" name="brand" defaultValue={brand} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="brand" defaultValue={brand} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text font-semibold">Dose</span></label>
-                        <input required type="text" name="dose" defaultValue={dose} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="dose" defaultValue={dose} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text font-semibold">Select a form</span></label>
@@ -49,21 +83,21 @@ const UpdateProduct = () => {
                 <div className="flex gap-2">
                     <div className="form-control">
                         <label className="label"><span className="label-text font-semibold">Generic</span></label>
-                        <input required type="text" name="generic" defaultValue={generic} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="generic" defaultValue={generic} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                     <div className="form-control">
                         <label className="label"><span className="label-text font-semibold">Price Per unit</span></label>
-                        <input required type="text" name="price_per_unit" defaultValue={price_per_unit} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="price_per_unit" defaultValue={price_per_unit} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                     <div className="form-control">
                         <label className="label"><span className="label-text font-semibold">Unit per strip (optional)</span></label>
-                        <input required type="text" name="pack_size_and_price" defaultValue={10} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="pack_size_and_price" defaultValue={10} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                 </div>
                 <div className="flex gap-2">
                     <div className="form-control">
                         <label className="label"><span className="label-text font-semibold">Producer</span></label>
-                        <input required type="text" name="company_name" defaultValue={company_name} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
+                        <input type="text" name="company_name" defaultValue={company_name} placeholder="Type here" className="input input-bordered input-warning w-full max-w-xs" />
                     </div>
                 </div>
                 <div>
